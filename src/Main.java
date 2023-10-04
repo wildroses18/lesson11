e23461a355bfb9206bf0bbe2784a91bff39366e4
StudentA415 <StudentA415@institut-it.mirea.ru>
2023-10-04 18:57:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int a= scanner.nextInt();
+        System.out.println((int) Math.pow(a,3));
+
+    }
+}
